7027d41889c6c0f9a1fc483b60cc7830fcc4f307
Cátia Oliveira <catia-96@hotmail.com>
2020-03-23 11:04:22 +0000

    Models are done and interface is half done. My goal is to show a list with every country and then information about the country the user selects.

---
+++ Paises/Modelos/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paises.Modelos
+{
+    public class Language
+    {
+        public string iso639_1 { get; set; }
+        public string iso639_2 { get; set; }
+        public string name { get; set; }
+        public string nativeName { get; set; }
+    }
+}
---
+++ Paises/Modelos/RegionalBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paises.Modelos
+{
+    public class RegionalBlock
+    {
+        public string acronym { get; set; }
+        public string name { get; set; }
+        public List<object> otherAcronyms { get; set; }
+        public List<object> otherNames { get; set; }
+    }
+}
